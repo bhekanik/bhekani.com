ce80dd968474d7ef0fe459c8c3bbc34d07316ec7
Bhekani Khumalo <bhekanik@gmail.com>
2024-01-04 23:49:31 +0000

    Add about and blog pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from "astro/config";
-
+import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://bhekani.com",
   output: "hybrid",
   adapter: vercel(),
+  integrations: [tailwind(), sitemap(), robotsTxt()]
 });
