d66c9faa04fff9df016b7cc51062f8702caf8f59
Bhekani Khumalo <bhekanik@gmail.com>
2024-01-05 16:48:32 +0000

    Update design on header and home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        headings: ["Abril Fatface", ...defaultTheme.fontFamily.sans],
+        body: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
+        code: ["JetBrains Mono", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
