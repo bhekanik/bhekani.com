29207af45beebfb170e7fa733ad5fcd81a26cd8d
Bhekani Khumalo <bhekanik@gmail.com>
2024-01-05 01:57:32 +0000

    Create blog using astro official tutorial

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,19 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function GET(context: any) {
+  const posts = await getCollection("posts");
+
+  return rss({
+    title: "Bhekani.com | Blog",
+    description: "Random things I learn",
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
