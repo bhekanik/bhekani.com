12d35070ddf99bc584050f8e4b1e81832e1f5a3a
Bhekani Khumalo <bhekanik@gmail.com>
2026-05-21 12:59:07 +0100

    fix: declare fontsource side-effect imports

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,8 @@
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module "@fontsource-variable/inter";
+declare module "@fontsource-variable/jetbrains-mono";
+declare module "@fontsource-variable/literata";
+declare module "@fontsource-variable/playfair-display";
